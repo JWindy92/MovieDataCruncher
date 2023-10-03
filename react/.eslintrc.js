1d95f4f3562cc8c50a985b1f5f93dad2759e665e
John Widmaier <johnwid92@gmail.com>
2023-10-03 09:08:28 -0400

    eslint config fix

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -1,15 +1,18 @@
-module.exports = {
-    "parser": "@babel/eslint-parser",
-    "rules": {}
-}
 // module.exports = {
-//     "env": {
-//         "browser": true
-//     },
-//     "plugins": [ "react" ],
-//     "extends": [
-//       "eslint:recommended",
-//       "plugin:react/recommended"
-//     ],
-//     "parser": "babel-eslint"
-// };
+//     "parser": "@babel/eslint-parser",
+//     "rules": {}
+// }
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "plugins": [ "react" ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ],
+    "parser": "@babel/eslint-parser",
+    "workingDirectories": [
+        {"mode": "auto"}   // Path to the shared code directory
+      ],
+};
