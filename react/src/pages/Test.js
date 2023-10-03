c666aff9626d1b5e15d7f3df6b71be3f2b4ef6a6
John Widmaier <johnwid92@gmail.com>
2023-10-03 10:10:57 -0400

    render sample data

--- react/src/pages/Test.js
+++ react/src/pages/Test.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import axios from 'axios'
+import sampleData from '../../../notes/example_movie_data.json';
 
-const Test = () => (
-  <div>
-    <h1>Test page</h1>
-    <p>This is the test page.</p>
-  </div>
-);
+// const GET_URL = ""
+
+function Test() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        console.log(sampleData.Results[0])
+        setData(sampleData.Results[0])
+        console.log(data.original_title)
+    })
+    // useEffect(() => {
+    //     axios.get(GET_URL).then(
+    //         response => {
+    //             setData(response.data)
+    //         }
+    //     ).catch(
+    //         error => console.error('Error fetching data: ', error)
+    //     )
+    // }, []); // The empty dependency array ensures this effect runs only once when the component is mounted)
+
+    return (
+        <div>
+            <h1>Movie Data</h1>
+            <strong>Title:</strong> {data["original_title"]}<br />
+            <strong>Overview:</strong> {data.overview}<br />
+            <strong>Rating:</strong> {data.vote_average}
+        </div>
+    );
+}
+
 
 export default Test;
